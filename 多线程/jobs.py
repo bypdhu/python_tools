3b1b0ee302a3c283e3ec6307e44425b6ff1fa244
bian <bian@test>
2017-11-30 10:25:42 +0800

    add test for 多线程

---
+++ 多线程/jobs.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+import datetime
+import requests
+# import time
+
+def testjob(*args):
+    start_time = datetime.datetime.now()
+    # time.sleep(2)
+    print(args)
+    end_time = datetime.datetime.now()
+
+    return args, (end_time - start_time).seconds
+    # return time.ctime(), args, (end_time - start_time).seconds
+
+
+def test_get_url(*args):
+    start_time = datetime.datetime.now()
+
+    url = 'http://10.99.70.52:5555/health'
+    res = requests.get(url)
+
+    end_time = datetime.datetime.now()
+    # if res.status_code != 200:
+    #     raise Exception
+    return args, (end_time - start_time).microseconds, res.status_code
